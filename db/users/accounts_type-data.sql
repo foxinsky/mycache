70bbf65975576abc98aaf6fdc941850871fec97a
foxinsky <oleksa.sky+dev@gmail.com>
2013-06-26 02:17:45 +0300

    Update account_types data

---
+++ db/users/accounts_type-data.sql
@@ -0,0 +1,9 @@
+--
+-- Default data for table `account_types`
+--
+
+INSERT INTO account_types (`acctype_id`, `acctype_str`, `acctype_descr`)
+VALUES
+(0, 'admin', 'Resource administrator'),
+(1, 'free', 'Free Account'),
+(2, 'premium', 'Premium Account');
